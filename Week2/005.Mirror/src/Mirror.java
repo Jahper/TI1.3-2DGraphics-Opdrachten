c40cfedda350349c3f3d9bb08aa0d10e9cf4e95c
Jahper <jaspertr04@gmail.com>
2024-02-08 20:43:16 +0100

    poging tot 2.5 gedaan, dunno of het goed is

--- Week2/005.Mirror/src/Mirror.java
+++ Week2/005.Mirror/src/Mirror.java
@@ -16,8 +16,7 @@ public class Mirror extends Application {
     ResizableCanvas canvas;
 
     @Override
-    public void start(Stage primaryStage) throws Exception
-    {
+    public void start(Stage primaryStage) throws Exception {
         BorderPane mainPane = new BorderPane();
         canvas = new ResizableCanvas(g -> draw(g), mainPane);
         mainPane.setCenter(canvas);
@@ -28,16 +27,47 @@ public class Mirror extends Application {
     }
 
 
-    public void draw(FXGraphics2D graphics)
-    {
+    public void draw(FXGraphics2D graphics) {
         graphics.setTransform(new AffineTransform());
         graphics.setBackground(Color.white);
         graphics.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
+
+        graphics.translate(canvas.getWidth() / 2, canvas.getHeight() / 2);
+        graphics.scale(1, -1);
+
+
+        //assenstelsel
+        GeneralPath path = new GeneralPath();
+        graphics.setColor(Color.BLACK);
+        path.moveTo(0, 0);
+        path.lineTo(0, 500);
+        path.lineTo(0, -500);
+        path.closePath();
+        path.lineTo(500, 0);
+        path.lineTo(-500, 0);
+
+        graphics.draw(path);
+
+//        graphics.drawRect(-50,100,100,100);
+        Rectangle2D rectangle2D = new Rectangle(-50, 100, 100, 100);
+        Point2D position = new Point2D.Double(0, -150);
+
+
+        double formula1 = (2 / ((1 + Math.pow(2.5, 2)))) - 1;
+        double formula2 = ((2 * 2.5) / (1 + Math.pow(2.5, 2)));
+        double formula3 = ((2 * Math.pow(2.5, 2)) / (1 + Math.pow(2.5, 2)));
+        AffineTransform tx = new AffineTransform(formula1, formula2, formula2, formula3,0,0);
+        tx.translate(position.getX(), position.getY());
+        tx.rotate(50);
+
+        graphics.draw(tx.createTransformedShape(rectangle2D));
+        graphics.draw(rectangle2D);
+
+
     }
 
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         launch(Mirror.class);
     }
 
