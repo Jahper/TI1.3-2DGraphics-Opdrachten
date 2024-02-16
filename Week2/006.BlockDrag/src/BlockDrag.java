6dc007abcb94f6303aefe74f26e201f9185565e8
Jahper <jaspertr04@gmail.com>
2024-02-16 17:20:28 +0100

    2.6 deels gemaakt

--- Week2/006.BlockDrag/src/BlockDrag.java
+++ Week2/006.BlockDrag/src/BlockDrag.java
@@ -1,11 +1,11 @@
 import java.awt.*;
 import java.awt.geom.*;
+import java.util.ArrayList;
 
 import javafx.application.Application;
 
 import static javafx.application.Application.launch;
 
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
@@ -15,10 +15,12 @@ import org.jfree.fx.ResizableCanvas;
 
 public class BlockDrag extends Application {
     ResizableCanvas canvas;
+    ArrayList<Renderable> renderables = new ArrayList<>();
+    FXGraphics2D graphics;
+    boolean dragging = false;
 
     @Override
-    public void start(Stage primaryStage) throws Exception
-    {
+    public void start(Stage primaryStage) throws Exception {
         BorderPane mainPane = new BorderPane();
         canvas = new ResizableCanvas(g -> draw(g), mainPane);
         mainPane.setCenter(canvas);
@@ -34,30 +36,69 @@ public class BlockDrag extends Application {
     }
 
 
-    public void draw(FXGraphics2D graphics)
-    {
+    public void draw(FXGraphics2D graphics) {
+        this.graphics = graphics;
         graphics.setTransform(new AffineTransform());
         graphics.setBackground(Color.white);
         graphics.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
+        graphics.clearRect(0, 0, 1920, 1080);
+
+        for (Renderable renderable : renderables) {
+            graphics.setColor(renderable.getColor());
+            graphics.fill(renderable.getShape());
+            graphics.draw(renderable.getShape());
+        }
     }
 
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         launch(BlockDrag.class);
     }
 
-    private void mousePressed(MouseEvent e)
-    {
+    private void mousePressed(MouseEvent e) {
+        for (int i = 0; i < renderables.size(); i++) {
+            Renderable renderable = renderables.get(i);
+            Color color = renderable.getColor();
+            graphics.setColor(color);
+            if (e.getY() >= renderable.getPosition().getY() - 25 && e.getY() <= renderable.getPosition().getY() + 25
+                    && e.getX() >= renderable.getPosition().getX() - 25 && e.getX() <= renderable.getPosition().getX() + 25) {
+                dragging = true;
+            }
+        }
+        System.out.println(dragging);
     }
 
-    private void mouseReleased(MouseEvent e)
-    {
-
+    private void mouseReleased(MouseEvent e) {
+        dragging = false;
     }
 
-    private void mouseDragged(MouseEvent e)
-    {
+    private void mouseDragged(MouseEvent e) {
+        for (int i = 0; i < renderables.size(); i++) {
+            Renderable renderable = renderables.get(i);
+            Color color = renderable.getColor();
+            graphics.setColor(color);
+            if (e.getY() >= renderable.getPosition().getY() - 25 && e.getY() <= renderable.getPosition().getY() + 25
+                    && e.getX() >= renderable.getPosition().getX() - 25 && e.getX() <= renderable.getPosition().getX() + 25
+            && !dragging) {
+                dragging = true;
+                renderable.setPosition(new Point2D.Double(e.getX(), e.getY()));
+                graphics.clearRect(0, 0, 1920, 1080);
+                renderables.set(i, new Renderable(color, new Point2D.Double(e.getX(), e.getY())));
+            }
+            for (Renderable renderable1 : renderables) {
+                graphics.setColor(renderable1.getColor());
+                graphics.fill(renderable1.getShape());
+                graphics.draw(renderable1.getShape());
+            }
+        }
+        dragging = false;
     }
-
-}
+        public void init () {
+            Color[] colors = {Color.red, Color.black, Color.blue, Color.yellow, Color.green};
+            for (int i = 0; i < 5; i++) {
+                double x = i * 50;
+                double y = i * 50;
+                renderables.add(new Renderable(colors[i], new Point2D.Double(x, 50)));
+            }
+        }
+    }
