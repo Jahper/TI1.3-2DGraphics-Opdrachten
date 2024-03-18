6df9522f2014d5608690bbf91d30e1d1915b180f
Jahper <jaspertr04@gmail.com>
2024-03-18 14:46:09 +0100

    bounce blok toegevoegd(*/ω＼*)

--- Eindopdracht/src/PinballFrame.java
+++ Eindopdracht/src/PinballFrame.java
@@ -76,38 +76,38 @@ public class PinballFrame {
         //side borders
         //left
         //bottom
-        createBodyAndGameObject(4, 30, new Vector2(-37.5, 41), "FrameImages/brickSideMediumLeft.png", new Vector2(-210, 150), 0.012);
+        createBodyAndGameObjectFrame(4, 30, new Vector2(-37.5, 41), "FrameImages/brickSideMediumLeft.png", new Vector2(-210, 150), 0.012);
 
         //bottom borders
         //left part 1
-        Body leftBottomBody = createBodyAndGameObject(25, 2.8, new Vector2(-28.5, 30), "FrameImages/sidePipePinballLeft.png", new Vector2(-290, 25), 0.012);
+        Body leftBottomBody = createBodyAndGameObjectFrame(25, 2.8, new Vector2(-28.5, 30), "FrameImages/sidePipePinballLeft.png", new Vector2(-290, 25), 0.012);
         leftBottomBody.rotate(0.4, new Vector2(-27.5, 30));
         //left part 2
-        Body leftBottomBody2 = createBodyAndGameObject(10, 2.8, new Vector2(-45, 30), "FrameImages/pipeSectionPinball.png", new Vector2(900, 25), 0.012);
+        Body leftBottomBody2 = createBodyAndGameObjectFrame(10, 2.8, new Vector2(-45, 30), "FrameImages/pipeSectionPinball.png", new Vector2(900, 25), 0.012);
         leftBottomBody2.rotate(0.4, new Vector2(-27.5, 30));
         //right
-        Body rightBottomBody = createBodyAndGameObject(25, 2.8, new Vector2(28.5, 30), "FrameImages/sidePipePinballRight.png", new Vector2(0, -25), 0.012);
+        Body rightBottomBody = createBodyAndGameObjectFrame(25, 2.8, new Vector2(28.5, 30), "FrameImages/sidePipePinballRight.png", new Vector2(0, -25), 0.012);
         rightBottomBody.rotate(-0.4, new Vector2(27.5, 30));
 
 
         //side borders
         //left
         //middle
-        createBodyAndGameObject(4, 40, new Vector2(-37.5, -10), "FrameImages/brickAndPipe.png", new Vector2(-30, 200), 0.012);
+        createBodyAndGameObjectFrame(4, 40, new Vector2(-37.5, -10), "FrameImages/brickAndPipe.png", new Vector2(-30, 200), 0.012);
         //top
-        createBodyAndGameObject(4, 10, new Vector2(-37.5, -45.5), "FrameImages/brickSideLeftSmall.png", new Vector2(-225, -50), 0.012);
+        createBodyAndGameObjectFrame(4, 10, new Vector2(-37.5, -45.5), "FrameImages/brickSideLeftSmall.png", new Vector2(-225, -50), 0.012);
 
         //right
-        createBodyAndGameObject(4, 50, new Vector2(40, 30), "FrameImages/brickSidePinballRightMedium.png", new Vector2(-225, 600), 0.012);
+        createBodyAndGameObjectFrame(4, 50, new Vector2(40, 30), "FrameImages/brickSidePinballRightMedium.png", new Vector2(-225, 600), 0.012);
 
         //side funnels
         //left
-        createBodyAndGameObject(4, 100, new Vector2(-47.5, 2), "FrameImages/brickSidePinballLeft.png", new Vector2(-235, 520), 0.012);
+        createBodyAndGameObjectFrame(4, 100, new Vector2(-47.5, 2), "FrameImages/brickSidePinballLeft.png", new Vector2(-235, 520), 0.012);
         //right
-        createBodyAndGameObject(4, 100, new Vector2(49.2, 2), "FrameImages/brickSidePinballRight.png", new Vector2(235, 400), 0.012);
+        createBodyAndGameObjectFrame(4, 100, new Vector2(49.2, 2), "FrameImages/brickSidePinballRight.png", new Vector2(235, 400), 0.012);
 
         //upper border
-        createBodyAndGameObject(100, 4, new Vector2(0.5, -50), "FrameImages/topBrickLayer.png", new Vector2(-131.5, -230), 0.012);
+        createBodyAndGameObjectFrame(100, 4, new Vector2(0.5, -50), "FrameImages/topBrickLayer.png", new Vector2(-131.5, -230), 0.012);
         //temporary bottom border for testing
         //fixme
         Body b = new Body();
@@ -119,10 +119,11 @@ public class PinballFrame {
         bodies.add(b);
 
         createFlippers(leftBottomBody, rightBottomBody);
+        createBounceThingies();
     }
 
     //method for returning a body for the borders of the pinball game
-    private Body createBodyAndGameObject(double width, double height, Vector2 vector2, String imageFile, Vector2 offset, double scale) {
+    private Body createBodyAndGameObjectFrame(double width, double height, Vector2 vector2, String imageFile, Vector2 offset, double scale) {
         Body body = new Body();
         BodyFixture bodyFixture = new BodyFixture(Geometry.createRectangle(width, height));
         bodyFixture.setFriction(0.7);
@@ -133,6 +134,18 @@ public class PinballFrame {
         bodies.add(body);
         objects.add(new GameObject(imageFile, body, offset, scale));
         return body;
+    }
+
+    private void createBounceThingies() {
+        Body entryBlock = new Body();
+        BodyFixture entryBlockFixture = new BodyFixture(Geometry.createSquare(6));
+        entryBlockFixture.setRestitution(10);
+        entryBlock.addFixture(entryBlockFixture);
+        entryBlock.setMass(MassType.INFINITE);
+        entryBlock.translate(new Vector2(47.5, -1.5));
+        entryBlock.rotate(0.9, new Vector2(47.5, -1.5));
+        bodies.add(entryBlock);
+
     }
 
     public ArrayList<GameObject> getObjects() {
